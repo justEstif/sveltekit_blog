240a30ae9dcec18327d4cbf87fd9ec3b4018db24
Estifanos <estifanosbeyene21@gmail.com>
2023-04-30 16:33:13 -0400

    feat: setup custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+	<title>Error</title>
+</svelte:head>
+
+<div class="error">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+	.error {
+		height: 100%;
+		display: grid;
+		place-content: center;
+	}
+</style>
